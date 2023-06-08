6f5f58f6d70b921b422a83509658e108db64ea72
Cassiano Elias Santana <201401665@senacinformatica.br>
2023-06-07 21:18:53 -0300

    Iniciando o JF

--- src/pi/reservas/PIReservas.java
+++ src/pi/reservas/PIReservas.java
@@ -5,6 +5,7 @@
 package pi.reservas;
 
 import controller.CCliente;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -21,8 +22,24 @@ public class PIReservas {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    public static int leiaNumInt(){
+        Scanner leiaNum = new Scanner(System.in);
+        try {
+            return leaiNum.nextInt();
+        } catch (InputMismatchException i){
+            System.out.println("Erro" + i.getMessage() + "\nTente novamente!");
+            leiaNumInt();
+        }
+        return 99;
+    }
  
-    public static void Menu(){
+    public static void MenuC(){
+        System.out.println("-- MENU RESERVAS --");
+        System.out.println("1 - Gerenciar Clientes");
+        System.out.println("2 - Gerenciar Mesas");
+        System.out.println("0 - Sair");
+        System.out.print("Digite Aqui: ");
         
     }
 }
